c066b024711531a833309a9a6e2ff72553908dca
Feiyu Zheng <feiyuzheng98@gmail.com>
2022-10-30 12:36:31 -0400

    ✨ feat: Update the code structure and add login for yelp account

--- test3.js
+++ test3.js
@@ -1,37 +1,71 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
+async function yelp_login(page, link, email, password){
+	await page.goto(link, {waitUntil: 'networkidle2'});
+
+	const emailField = await page.$('form[action*=login] > #email');
+	await emailField.type(email, {delay: 100});
+
+	const passwordField = await page.$('form[action*=login] > #password');
+	await passwordField.type(password, {delay: 100});
+
+	await passwordField.press("Enter");
+	await page.waitForNavigation({waitUntil: 'networkidle2'});
+
+	await page.screenshot({path: './screenshot.png', fullPage: true});
+	return page
+}
+
+async function puppeteerInit(){
 	const args = puppeteer.defaultArgs().filter(arg => arg !== '--enable-asm-webassembly');
 	args.push('--enable-webgl-draft-extensions', '--shared-array-buffer');
 	const browser = await puppeteer.launch({ ignoreDefaultArgs: true, args });
 	const page = await browser.newPage();
 	await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3419.0 Safari/537.36');
-	await page.goto('https://www.yelp.com', {waitUntil: 'networkidle2'});
+	return [browser, page];
+}
+
+async function yelp(page, login=false){
+	// todo: these parameters should be read from a configuration file later
+	const yelp_home = 'https://www.yelp.com';
+	const email = "";
+	const password = "";
+
+	await page.goto(yelp_home, {waitUntil: 'networkidle2'});
+	
 	const hrefs = await page.evaluate(() => {
 		return Array.from(document.getElementsByTagName('a'), a => a.href);
 	});
+	
+	if(login){
+		process.stdout.write("Extracting login link...");
+		const login_href = hrefs.filter(x => x.search('login') != -1)[0];
+		process.stdout.write(colorizeString("done!\n", "green"));
+
+		page = await yelp_login(page, login_href, email, password);
+	}
+	
 	/*for(let i =0;i<hrefs.length;i++){
 			  console.log(i);
 			  console.log(hrefs[i]);
 		  }*/
-	for(let i =0;i<hrefs.length;i++){
+	
+	for(let i = 0; i < hrefs.length; i++){
 		// console.log(hrefs[i]=='');
-		if(hrefs[i]==''){
+		if(hrefs[i] == ''){
 			continue
 		}
-		if(hrefs[i]==' ' || hrefs[i].indexOf("//www.yelp.com")==-1||hrefs[i].indexOf("pdf")>1){
+		if(hrefs[i] == ' ' || hrefs[i].indexOf("//www.yelp.com") == -1 || hrefs[i].indexOf("pdf") > 1){
 			continue;
 		}
-		await page.goto(hrefs[i],{
-			'timeout': 60000
-		});
+		await page.goto(hrefs[i], {'timeout': 60000});
 		//await page.goBack();
 		var cur=await page.evaluate(() => {
 			return Array.from(document.getElementsByTagName('a'), a => a.href);
 		});
 
-		if(hrefs.length<20000){
-			hrefs.push.apply(hrefs,cur);
+		if(hrefs.length < 20000){
+			hrefs.push.apply(hrefs, cur);
 		}/*else{
 			const fs = require('fs');
 			for(let i =0;i<hrefs.length;i++){
@@ -43,9 +77,16 @@ const puppeteer = require('puppeteer');
 				});
 			}
 		}*/
-		console.log(i);
-		console.log(hrefs[i]);
+		console.log(i + " " + hrefs[i]);
 	}
-	//console.log(hrefs);
+}
+
+async function main(){
+	const [browser, page] = await puppeteerInit();
+	
+	await yelp(page, login=false);
+	
 	await browser.close();
-})();
+}
+
+main();
