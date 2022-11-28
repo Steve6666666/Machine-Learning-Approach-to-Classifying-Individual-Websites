0972145f891730e0658f76a5b1fa44a578e6b2e5
Feiyu Zheng <feiyuzheng98@gmail.com>
2022-11-28 02:00:49 -0500

    ✨ feat: Add login for Target

--- main.js
+++ main.js
@@ -616,6 +616,96 @@ async function reddit_login(page, website, hrefs, email, password, type_delay_pe
 	return page
 }
 
+/**
+ * Target login code (TODO)
+ * @param {puppeteer.Page} page - puppeteer Page instance
+ * @param {String} website - website name
+ * @param {String} hrefs - links from home page
+ * @param {String} email - account email
+ * @param {String} password - account password
+ * @param {Number} type_delay_per_char - type speed in ms
+ * @param {Boolean} debug - debug mode
+ * @return {puppeteer.Page} - redirected page after login
+ */
+async function target_login(page, website, hrefs, email, password, type_delay_per_char=200, debug=false){
+
+	let websiteDebugDir = DEBUG_DIR + website + "/";
+	if(debug){
+		if(!fs.existsSync(websiteDebugDir)){
+			fs.mkdirSync(websiteDebugDir, {recursive: true});
+		}
+	}
+
+	process.stdout.write("Extracting login link...");
+	const login_link = "https://www.target.com/account"
+	process.stdout.write(colorizeString("done!\n", "green"));
+
+	process.stdout.write("Navigating to login link...");
+	await page.goto(login_link, {waitUntil: 'networkidle2'});
+	process.stdout.write(colorizeString("done!\n", "green"));
+
+	process.stdout.write("Filling up email...");
+	const emailField = await page.$("#username");
+	await emailField.type(email, {delay: type_delay_per_char});
+	process.stdout.write(colorizeString("done!\n", "green"));
+
+	process.stdout.write("Filling up password...");
+	const passwordField = await page.$('#password');
+	await passwordField.type(password, {delay: type_delay_per_char});
+	process.stdout.write(colorizeString("done!\n", "green"));
+
+	console.log(
+		"Input email: " +
+		colorizeString(await emailField.evaluate(e => e.value), "yellow")
+	);
+	console.log(
+		"Input password: " +
+		colorizeString(await passwordField.evaluate(e => e.value), "yellow")
+	);
+
+	// process.stdout.write("Submitting login request by clicking sign in button and waiting for navigation...");
+	// await page.click("#login")
+
+	process.stdout.write("Submitting login request by pressing Enter and waiting for navigation...");
+	await passwordField.press("Enter");
+	// await page.waitForNavigation({waitUntil: 'networkidle2'});
+	
+	let mobileSkip = null;
+	try{
+		mobileSkip = await page.waitForSelector("a[href='/']", {timeout: 5000});
+		if(mobileSkip != null){
+			mobileSkip.click();
+		}
+	}
+	catch(e){
+		console.log(e);
+	}
+
+	if(debug){
+		await page.screenshot({path: websiteDebugDir + website + "_1.png", fullPage: true});
+	}
+	
+	let circleSkip = null;
+	try{
+		circleSkip = await page.waitForSelector("#circle-skip", {timeout: 5000});
+		if(circleSkip != null){
+			circleSkip.click();
+		}
+	}
+	catch(e){
+		console.log(e);
+	}
+	
+	process.stdout.write(colorizeString("done!\n", "green"));
+
+	console.log("Login complete!");
+
+	if(debug){
+		await page.screenshot({path: websiteDebugDir + website + "_final.png", fullPage: true});
+	}
+	return page
+}
+
 async function main(){
 	let login = false;
 
