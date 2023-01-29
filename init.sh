56d9850ffc7de469c96f98b1852c4ca3c410f65e
Steve6666666 <song_chu_xu@163.com>
2023-01-30 00:41:36 +0800

    pip3 install

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@ nvm install 17 -y
 sudo apt-get update -y
 sudo apt-get install -y man-db
 sudo apt-get install -y chromium-browser
+sudo apt install python3-pip
+pip3 install dpkt
 
 ~/.nvm/versions/node/v17.9.1/bin/npm i puppeteer
 
