bfb93b5159486cc8e692f0c7368495e850d2eaff
Steve6666666 <song_chu_xu@163.com>
2023-03-02 11:21:09 -0500

    fix error

--- main.js
+++ main.js
@@ -136,7 +136,7 @@ async function crawl(page, website, login=false){
                		 });
 	}
 }
-async function shuffleArray(array) {
+function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [array[i], array[j]] = [array[j], array[i]];
