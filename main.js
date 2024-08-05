6fe15155c31d730a664a0095da2be982e48fb3a1
Steve Song <song_chu_xu@163.com>
2024-08-05 15:17:10 -0400

    video site

--- main.js
+++ main.js
@@ -97,9 +97,9 @@ async function crawl(page, website, login=false){
 	console.log("Crawlling begins...")
 	
 	//await normal(page, website, hrefs, fs);
-	// await video_site(page, website, hrefs, fs);
+	await video_site2(page, website, hrefs, fs);
 	const numbers = await readNumbersFromFile('tiktok.txt');
-	await video_site(page, website, hrefs, fs, numbers);
+	// await video_site(page, website, hrefs, fs, numbers);
 
 	// for(let i = 0; i < hrefs.length; i++){
 	// 	var begin=Date.now();
@@ -277,6 +277,49 @@ async function video_site(page, website, hrefs, fs,numbers){
         });
     }
 }
+async function video_site2(page, website, hrefs, fs){
+	for (let i = 0; i < hrefs.length; i++) {
+		var begin = Date.now();
+		if (hrefs[i] == '') {
+			continue;
+		}
+		if (hrefs[i] == ' ' || hrefs[i].indexOf("//www." + website + ".com") == -1 || hrefs[i].indexOf("pdf") > 1) {
+			continue;
+		}
+		try {
+			await page.goto(hrefs[i], { 'timeout': LINK_TIMEOUT });
+			// Check for video elements on the page
+			const hasVideo = await page.evaluate(() => {
+				const videoElements = document.getElementsByTagName('video');
+				return videoElements.length > 0;
+			});
+			console.log(hasVideo)
+			if (hasVideo) {
+				// Play the video if it exists
+				await page.evaluate(() => {
+					const videoElements = document.getElementsByTagName('video');
+					for (let video of videoElements) {
+						video.play();
+					}
+				});
+				// Wait for a certain time to simulate watching the video
+				await page.waitForTimeout(5000); // Adjust the time as needed
+			}
+	
+			var cur = await page.evaluate(() => {
+				return Array.from(document.getElementsByTagName('a'), a => a.href);
+			});
+			cur = shuffleArray(cur);
+		} catch (e) {
+			console.log(e.message);
+		}
+	
+		if (hrefs.length < 20000) {
+			hrefs.push.apply(hrefs, cur);
+		}
+	}	
+}
+
 
 
 
