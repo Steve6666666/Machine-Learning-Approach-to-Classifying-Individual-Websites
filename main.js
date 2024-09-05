9d8c669a24ad09f0c6964e29de3d9e0d5d162661
Steve Song <song_chu_xu@163.com>
2024-09-05 14:14:28 -0400

    video site

--- main.js
+++ main.js
@@ -406,7 +406,7 @@ async function youtube(page, website, hrefs, fs,numbers){
 			await page.waitForSelector('input[name="search_query"]');
 			await page.type('input[name="search_query"]', 'Puppeteer 教程', { delay: 100 });
 			await page.waitForTimeout(1000)
-			await page.keyboard.press('Enter');
+			await page.click('#search-icon-legacy'); 
 			await page.waitForNavigation({ waitUntil: 'networkidle0' });
 			console.log('Search results page URL:', page.url());
 			const firstElementPosition = await page.evaluate(() => {
